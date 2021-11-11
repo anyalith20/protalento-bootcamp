6f8d97a234b06ba62198b8c29c1c64f5fdc26cfe
anyalith <93748613+anyalith@users.noreply.github.com>
2021-11-11 17:58:15 -0500

    Clase 3

---
+++ pruebas/src/main/java/pruebas/Operadores.java
@@ -0,0 +1,28 @@
+package pruebas;
+
+public class Operadores {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//ctrl+d comando para eliminar una linea
+		
+		//*matematicos 
+		
+		int edad1 = 10;
+		int edad2= 20;
+		int res = edad1 + edad2;
+		System.out.println("La suma de edad + edad2 es ="+res);
+		System.out.println(res);
+		
+		
+		String nombre = "Carlos";
+		String apellido = "Lopez";
+		
+
+		
+		System.out.println();
+
+	}
+
+}
